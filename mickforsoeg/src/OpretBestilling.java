a904e31a146726a7128f7ac1e43ee628ef8ffb20
MivleDK <grevmivlos@gmail.com>
2019-10-24 15:12:10 +0200

    tilføje filer til forsøg

---
+++ mickforsoeg/src/OpretBestilling.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class OpretBestilling extends readtest {
+
+    int i = 2;
+
+    @Override
+    public String readMenu(int i) throws IOException {
+        return super.readMenu(i);
+    }
+
+}
--- mickforsoeg/src/readtest.java
+++ mickforsoeg/src/readtest.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class readtest {
 
-    public static void main(String[] args) throws IOException {
+    protected String PizzaNo;
+
+    public String readMenu(int i) throws IOException {
         BufferedReader objReader = null;
-
         String strCurrentLine;
-
         objReader = new BufferedReader(new FileReader("C:\\Users\\mla\\Google Drev\\Datamatiker\\1_semester\\Semesteropgave_Marios pizzabar\\menu2.txt"));
 
         ArrayList<String> menukortArr = new ArrayList<>();
@@ -18,7 +17,6 @@ public class readtest {
         while ((strCurrentLine = objReader.readLine()) != null){
             menukortArr.add(strCurrentLine);
         }
-        System.out.println(menukortArr);
-        System.out.println(menukortArr.get(12));
+        return menukortArr.get(i);
     }
 }
