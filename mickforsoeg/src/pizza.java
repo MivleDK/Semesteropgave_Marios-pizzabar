717855caf8e7309e95a3f2ece64acba833c16bf6
Mick <grevmivlos@gmail.com>
2019-10-24 11:04:17 +0200

    Forsøg med readfile. Scanner ville ikke læse en lang menu - det ville buffered reader

--- mickforsoeg/src/pizza.java
+++ mickforsoeg/src/pizza.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class pizza {
@@ -8,7 +9,7 @@ public class pizza {
     public static void main(String[] args) throws FileNotFoundException {
 
         //Indlæs menukort filen
-        File menukort = new File("C:\\Users\\mla\\Google Drev\\Datamatiker\\1_semester\\Semesteropgave_Marios pizzabar\\menukort.txt");
+        File menukort = new File("C:\\Users\\mla\\Google Drev\\Datamatiker\\1_semester\\Semesteropgave_Marios pizzabar\\menu2.txt");
 
         //init ArrayList som indeholder data fra menukort filen
         ArrayList<String> menukortArr = new ArrayList<>();
@@ -18,11 +19,11 @@ public class pizza {
             while (sc.hasNext()) {
                 menukortArr.add(sc.nextLine());
             }
+            sc.close();
         }
 
         //Print menukort
         System.out.println(menukortArr);
-
     }
 }
 
