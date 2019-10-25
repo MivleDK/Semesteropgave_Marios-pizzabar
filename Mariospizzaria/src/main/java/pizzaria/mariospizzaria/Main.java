d8635f1a92b8669859fb512181bd1dea827ec2d7
Amazingh0rse <therahbek@gmail.com>
2019-10-25 11:38:58 +0200

    merged mickforsøg

--- Mariospizzaria/src/main/java/pizzaria/mariospizzaria/Main.java
+++ Mariospizzaria/src/main/java/pizzaria/mariospizzaria/Main.java
@@ -1,24 +1,10 @@
-/*
- CPH Business 2019
-Semesteropgave Marios-pizzabar
-Alexander Pihl, Benjamin Iglesias, Mick Larsen, Morten Rasmussen
- */
-package pizzaria.mariospizzaria;
-
 import java.io.IOException;
 
-/*
- * @author Amazingh0rse
- */
-public class Main {
+public class main{
 
     public static void main(String[] args) throws IOException {
 
-        Menu newMenu = new Menu();
-        newMenu.visMenu();
-        newMenu.startMenu();
-        //bestillingsMenu bestillingsmenu = new bestillingsMenu();
-        //bestillingsmenu.visBestillingsMenu
+        bestillingsMenu bestillingsmenu = new bestillingsMenu();
+        bestillingsmenu.visBestillingsMenu();
     }
-
 }
--- Mariospizzaria/src/main/java/pizzaria/mariospizzaria/Menukort.java
+++ Mariospizzaria/src/main/java/pizzaria/mariospizzaria/Menukort.java
@@ -1,13 +1,48 @@
-/*
- CPH Business 2019
-Semesteropgave Marios-pizzabar
-Alexander Pihl, Benjamin Iglesias, Mick Larsen, Morten Rasmussen
+/**@author: Mick Larsen
+ * menukort klasen læser menukortet fra en fil.
  */
-package pizzaria.mariospizzaria;
 
-/*
- * @author Amazingh0rse
- */
-public class Menukort {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class menukort {
+
+    //Init ArrayList til at holde menukortet
+    protected ArrayList<String> menukortArr = new ArrayList<>();
+
+    /**Forudsætter: .txt fil på disk med én pizza på hver linje.
+     * Hvert element på linjen skal være separeret med mellemrum
+     * Hver linje skal starte med <pizza nr> <pizza navn> <pizza pris> <pizza ingrediens 1> <pizza ingrediens 2> <pizza ingrediens osv.>
+     *
+     *Metodekald: Ingen parametre
+     *
+     * Output:
+     * */
+    public menukort() throws IOException {
+        BufferedReader objReader = null;
+        String strCurrentLine;
+        objReader = new BufferedReader(new FileReader("C:\\Users\\mla\\Google Drev\\Datamatiker\\1_semester\\Semesteropgave_Marios pizzabar\\menu2.txt"));
+
+        while ((strCurrentLine = objReader.readLine()) != null){
+            menukortArr.add(strCurrentLine);
+        }
+    }
+
+    protected String getPizza(int i){
+
+        return menukortArr.get(i-1);
+    }
+
+    public double getPris(int n) throws IOException{ //Implementer scanner
+        n= n-1 ;
+        String test = (String) menukortArr.get(n);
+        String[] splited = test.split("\\s+");
+        String temp = splited[2];
+        double pris = Double.parseDouble( temp );
+
+        return pris;
+    }
 
 }
