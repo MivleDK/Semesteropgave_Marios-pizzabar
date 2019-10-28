ce601797b6e6ab2c068c1cd7c0bf6cb909a45b8a
AlexanderPihl <cph-as509@cphbusiness.dk>
2019-10-28 14:10:47 +0100

    opdatering

---
+++ Semesteropgave_Marios-pizzabar/Marios_Pizzaria/src/marios_pizzaria/Menu.java
@@ -0,0 +1,90 @@
+/*
+ CPH Business 2019
+Semesteropgave Marios-pizzabar
+Alexander Pihl, Benjamin Iglesias, Mick Larsen, Morten Rasmussen
+ */
+package marios_pizzaria;
+
+/*
+ * @author Amazingh0rse
+ */
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static Scanner scan = new Scanner(System.in);
+
+    public void visMenu() {
+        System.out.println("****************************************************************************************");
+        System.out.println("*  __  __               _                        _                  _                  *");
+        System.out.println("* |  \\/  |             (_)                      (_)                | |                 *");
+        System.out.println("* | \\  / |  __ _  _ __  _   ___   ___     _ __   _  ____ ____ __ _ | |__    __ _  _ __ *");
+        System.out.println("* | |\\/| | / _` || '__|| | / _ \\ / __|   | '_ \\ | ||_  /|_  // _` || '_ \\  / _` || '__|*");
+        System.out.println("* | |  | || (_| || |   | || (_) |\\__ \\   | |_) || | / /  / /| (_| || |_) || (_| || |   *");
+        System.out.println("* |_|  |_| \\__,_||_|   |_| \\___/ |___/   | .__/ |_|/___|/___|\\__,_||_.__/  \\__,_||_|   *");
+        System.out.println("*                                        | |                                           *");
+        System.out.println("*                                        |_|                                           *");
+        System.out.println("****************************************************************************************");
+        System.out.println("1 Tilføj Bestilling");
+        System.out.println("2 Fjern Bestilling");
+        System.out.println("3 Vis menukort");
+        System.out.println("4 Vis statistik");
+        System.out.println();
+        System.out.println("Q Afslut");
+        System.out.println("****************************************************************************************");
+    }
+
+    public void startMenu() throws IOException {
+        String valg = "1";
+        valg = getInput();
+
+        switch (valg) {
+            case "1":// deal med s
+                BestillingsMenu bestillingsmenu = new BestillingsMenu();
+                bestillingsmenu.visBestillingsMenu();
+                valg = "q";
+                break;
+            case "2":
+                System.out.println("Virker ikke endnu");
+                valg = "q";
+                break;
+            case "3":
+                System.out.println("Virker ikke endnu");
+                valg = "q";
+                break;
+
+            /* @MLA: Viser menukortet i en GUI for sig selv.
+                    ShowMenuKort newMenuKort = new ShowMenuKort();
+                    newMenuKort.ShowMenuKort();
+                    visMenu();
+                    valg = getInput();
+             */
+            case "4":
+                Statistik vis = new Statistik();
+                vis.visStatistik();
+                //System.out.println("Virker ikke endnu");
+                //valg = "q";
+                break;
+
+            case "q":
+                //valg = "q";
+                System.exit(0);
+                break;
+            default:
+                valg = "z";
+                System.out.println("Vælg et punkt der findes på menuen");
+                startMenu();
+        }
+
+        visMenu();
+        startMenu();
+
+    }
+
+    public static String getInput() {
+        String valg = scan.nextLine();
+        return valg;
+    }
+
+}
---
+++ Semesteropgave_Marios-pizzabar/Marios_Pizzaria/src/marios_pizzaria/ShowMenuKort.java
@@ -0,0 +1,51 @@
+/*
+ CPH Business 2019
+Semesteropgave Marios-pizzabar
+Alexander Pihl, Benjamin Iglesias, Mick Larsen, Morten Rasmussen
+ */
+package marios_pizzaria;
+
+/*
+ * @author Morten Rasmussen
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class ShowMenuKort {
+
+    public void ShowMenuKort() {
+        // Opretter data til tabellen
+        String[] headings
+                = new String[]{"Nummer", "Navn", "Pris", "Ingrediens", "Ingrediens", "Ingrediens", "Ingrediens", "Ingrediens", "Ingrediens", "Ingrediens"};
+        Object[][] data = new Object[][]{ //opretter et object med data
+            {"1", "Vesuvio", "57", "Tomatsauce", "Ost", "Skinke", "Oregano", " ", " ", " "},
+            {"2", "Amerikaner", "53", "Tomatsauce", "Ost", "Oksefars", "Oregano", " ", " ", " "},
+            {"3", "Cacciatore", "57", "Tomatsauce", "Ost", "Pepperoni", "Oregano", " ", " ", " "},
+            {"4", "Carbona", "63", "Tomatsauce", "Ost", "Kødsauce", "Spaghetti", "Cocktailpølser", "Oregano", " "},
+            {"5", "Dennis", "65", "Tomatsauce", "Ost", "Skinke", "Pepperoni", "Cocktailpølser", "Oregano", " "},
+            {"6", "Bertil", "57", "Tomatsauce", "Ost", "Bacon", "Oregano", " ", " ", " "},
+            {"7", "Silvia", "61", "Tomatsauce", "Ost", "Pepperoni", "Rød Peber", "Løg", "Oliven", "Oregano"},
+            {"8", "Victoria", "61", "Tomatsauce", "Ost", "Skinke", "Ananas", "Champignon", "Løg", "Oregano"},
+            {"9", "Toronfo", "61", "Tomatsauce", "Ost", "Skinke", "Bacon", "Kebab", "Chili", "Oregano"},
+            {"10", "Capricciosa", "61", "Tomatsauce", "Ost", "Skinke", "Champignon", "Oregano", " ", " "},
+            {"11", "Hawai", "61", "Tomatsauce", "Ost", "Skinke", "Ananas", "Oregano", " ", " "},
+            {"12", "Le Blissola", "61", "Tomatsauce", "Ost", "Skinke", "Rejer", "Oregano", " ", " "},
+            {"13", "Venezia", "61", "Tomatsauce", "Ost", "Skinke", "Bacon", "Oregano", " ", " "},
+            {"14", "Mafia", "61", "Tomatsauce", "Ost", "Pepperoni", "Løg", "Oregano", " ", " "},};
+
+        // Opretter et JTable med dataen fra tabellen
+        JTable table = new JTable(data, headings);
+
+        JFrame frame = new JFrame("Marios Pizzabar");
+        frame.add(new JScrollPane(table));
+
+        ///*  Lukker programmet hvis du lukker vinduet
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        // */
+        frame.setSize(800, 300);            //Sætter dimensions på Jframe
+        frame.setVisible(true);
+    }
+}
